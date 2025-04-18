9f2e4290f41fd9debffcb1ae99c7a26df75a31be
leotao0608 <leotao0608@gmail.com>
2025-04-17 20:50:42 -0600

    updated

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function set_mode(diff){//difficulty
+    document.getElementById("button_container").style.display="none";
+    if(diff==0){        //easy
 
+    }else if(diff==1){  //medium
+
+    }else{              //hard
+
+    }
+}
